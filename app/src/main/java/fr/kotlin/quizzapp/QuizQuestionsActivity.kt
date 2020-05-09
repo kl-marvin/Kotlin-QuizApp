abb7b34445dbe1a872e840e195ac1dd67b9fa639
Marvin K. Langlois <klm@macbook-pro-de-marvin.home>
2020-05-09 12:26:38 +0200

    using model in the view

--- app/src/main/java/fr/kotlin/quizzapp/QuizQuestionsActivity.kt
+++ app/src/main/java/fr/kotlin/quizzapp/QuizQuestionsActivity.kt
@@ -3,6 +3,7 @@ package fr.kotlin.quizzapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 import kotlin.math.log
 
 class QuizQuestionsActivity : AppCompatActivity() {
@@ -13,5 +14,21 @@ class QuizQuestionsActivity : AppCompatActivity() {
 
         val questionsList = Constants.getQuestions()
         Log.i("TEST", questionsList.toString())
+
+
+        val currentPosition = 1
+        val question : Question? = questionsList[currentPosition -1] // [0]
+
+        progressBar.progress = currentPosition
+        tv_progress.text = "$currentPosition" + "/" + progressBar.max
+
+        tv_question.text = question!!.question
+        iv_flag.setImageResource(question.image)
+        tv_option_one.text = question.option1
+        tv_option_two.text = question.option2
+        tv_option_three.text = question.option3
+        tv_option_four.text = question.option4
+
+
     }
 }
